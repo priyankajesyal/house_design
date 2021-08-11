79e6d454afd7e7b8416998a1694ec948829325fd
abhishekpathania1 <abhishekpathaniawins@gmail.com>
2021-08-11 17:33:27 +0530

    few changes

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -6,12 +6,8 @@
             <div class="offset-4 col-md-4 ">
                 <h3 class="text-center">USER PROFILE</h3>
                 <div class="card">
-                    <div class="text-center details col-md-10 offset-1"> 
+                    <div class="text-center details col-md-10 offset-1">
                         <br>
-                        <div class="text-center image">
-                            <img src="{{ $data->profile_photo_url }}" class="img-fluid img-thumbnail "
-                                alt="{{ $data->name }}" width="200px" height="200px">
-                        </div>
                         <hr>
                         <label for="name" style="font-weight:900"> USER:</label>
                         <input type="text" class="form-control" name="name" value="{{ $data->name }}" readonly>
